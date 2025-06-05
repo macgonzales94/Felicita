5e20cb304a634b56581e8bc1e9d7d5c6f07121c5
Miguel Antonio Cuadros Gonzales <ma.cgonzales.94@gmail.com>
2025-06-05 00:51:03 -0500

    Mejorando

---
+++ backend/felicita/__init__.py
@@ -0,0 +1,8 @@
+"""
+FELICITA - Sistema de Facturación Electrónica para Perú
+Proyecto principal Django
+"""
+
+__version__ = '1.0.0'
+__author__ = 'Equipo FELICITA'
+__email__ = 'desarrollo@felicita.pe'
